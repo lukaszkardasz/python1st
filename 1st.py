bfddc64cd051d91a7951b8eefe869a6445b7e38c
lkardasz <kardasz.lukasz@gmail.com>
2024-10-27 21:50:06 +0100

    add strings and input

--- 1st.py
+++ 1st.py
@@ -1,2 +1,21 @@
-text = "hello world"
+from math import sqrt
+import pathlib
+
+
+text = sqrt(4)
 print(text)
+
+print("Aktualna ścieżka: " + str(pathlib.Path.cwd()))
+
+imie = ("Łukasz")
+
+imie = imie.upper()
+print(imie)
+
+a = int(input("Podaj liczbę a: "))
+print("liczba a:",a)
+
+b = int(input("Podaj liczbę b: "))
+print("liczba b:", b)
+
+print("wynik dodawania", a, " + ", b, "=",  a + b)
